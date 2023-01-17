4052279073efeb0f0f57e972833a5596260fff13
parcellaann <anna.betik@mail.ru>
2023-01-17 19:24:22 +0300

    add solution

--- Homework 3.6/Homework 3.6.cpp
+++ Homework 3.6/Homework 3.6.cpp
@@ -5,16 +5,35 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Task 1\n\n";
+	int a = 5;
+	for (int i = 1; i <= 10; i++)
+		std::cout << a << " * " << i << " = " << a * i << "\n";
+
+	std::cout << "\nTask 2\n";
+	int number = 0;
+	while (number != 7)
+	{
+		std::cout << "Enter your number: ";
+		std::cin >> number;
+		if (number < 7)
+			std::cout << number <<" < 7\n";
+		else if (number > 7)
+			std::cout << number << " > 7\n";
+		if (number < 10)
+			std::cout << number << " < 10\n";
+		else if (number > 10)
+			std::cout << number << " > 10\n";
+		if (number % 2 == 0)
+			std::cout << "The number "<< number << " is divisible by 2 without remainder.\n";
+		else
+			std::cout << "The number " << number << " isn't divisible by 2 without remainder.\n";
+		if (number % 3 == 0)
+			std::cout << "The number " << number << " is divisible by 3 without remainder.\n";
+		else
+			std::cout << "The number " << number << " isn't divisible by 3 without remainder.\n";
+
+		std::cout << "To exit, enter the number 7.\n\n";
+	}
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
